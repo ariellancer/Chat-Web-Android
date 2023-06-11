1617423bb6bd5836b7ec008b657e3701272150aa
ariellancer <ariel.lancer@gmail.com>
2023-06-11 15:38:37 +0300

    updating the register page

--- app/src/main/java/com/example/exe3/Register.java
+++ app/src/main/java/com/example/exe3/Register.java
@@ -1,27 +1,55 @@
 package com.example.exe3;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity{
+    private static final int REQUEST_CODE_PICK_IMAGE = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         Button btnRegister=findViewById(R.id.btnRegister);
+        Button btnChoosePic= findViewById(R.id.choosePic);
+        imageView = findViewById(R.id.imageView);
+
+
+        btnChoosePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open file picker
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+            }
+        });
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageURI(selectedImageUri);
+        }
     }
 
 
